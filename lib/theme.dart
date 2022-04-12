1b28b0f5d78473293575eb45af4551fdcdf88562
Shreesh Tripathi <shreeshtripathi05@gmail.com>
2022-04-13 01:05:45 +0530

    theme updated

---
+++ lib/theme.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppColors {
+  static const secondary = Color(0xff06B460);
+  static const accent = Color(0xFF067558);
+  static const textDark = Color.fromARGB(255, 78, 88, 85);
+  static const textLight = Color(0xFFF5F5F5);
+  static const textFaded = Color.fromARGB(255, 155, 180, 173);
+  static const iconLight = Color(0xFFB1B4C0);
+  static const iconDark = Color(0xFFB183C1);
+  static const textHighlight = secondary;
+  static const cardLight = Color(0xFFF9FAFE);
+  static const cardDark = Color(0xFF303334);
+}
+
+abstract class _LightColors {
+  static const background = Colors.white;
+  static const card = AppColors.cardLight;
+}
+
+abstract class _DarkColors {
+  static const background = Color(0xFF1B1E1F);
+  static const card = AppColors.cardDark;
+}
+
+abstract class AppTheme {
+  static const accentColor = AppColors.accent;
+  static final visualDensity = VisualDensity.adaptivePlatformDensity;
+
+  static ThemeData light(ThemeData theme) => ThemeData(
+        brightness: Brightness.light,
+        accentColor: accentColor,
+        visualDensity: visualDensity,
+        textTheme: GoogleFonts.mulishTextTheme(theme.textTheme)
+            .apply(bodyColor: AppColors.textDark),
+        backgroundColor: _LightColors.background,
+        scaffoldBackgroundColor: _LightColors.background,
+        cardColor: _LightColors.card,
+        primaryTextTheme: const TextTheme(
+          headline6: TextStyle(color: AppColors.textDark),
+        ),
+        iconTheme: const IconThemeData(color: AppColors.iconDark),
+      );
+
+  static ThemeData dark(ThemeData theme) => ThemeData(
+        brightness: Brightness.dark,
+        accentColor: accentColor,
+        visualDensity: visualDensity,
+        textTheme: GoogleFonts.interTextTheme(theme.textTheme)
+            .apply(bodyColor: AppColors.textLight),
+        backgroundColor: _DarkColors.background,
+        scaffoldBackgroundColor: _DarkColors.background,
+        cardColor: _DarkColors.card,
+        primaryTextTheme: const TextTheme(
+          headline6: TextStyle(color: AppColors.textLight),
+        ),
+        iconTheme: const IconThemeData(color: AppColors.iconLight),
+      );
+}
